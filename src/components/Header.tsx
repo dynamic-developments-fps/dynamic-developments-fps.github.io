7f50e191b9371bd5646fd72945417f7154dc9d91
Boxy <nozakisola@gmail.com>
2025-11-10 21:40:27 -0600

    Discord Embeddable

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,27 +7,29 @@ export function Header() {
 	return (
 		<header>
 			<nav>
-				<a href="/" className={"link-nav " + (url == '/' && 'active-nav')}>
+				<a href="/" className={"link-nav stroke-lite " + (url == '/' && 'active-nav')}>
 					Home
 				</a>
-				<a href="/404" className={"link-nav " + (url == '/404' && 'active-nav')}>
+				<a href="/404" className={"link-nav stroke-lite " + (url == '/404' && 'active-nav')}>
 					404
 				</a>
 				<span className="right">
 					<a href="https://www.youtube.com/@DynamicDevelopmentsFPS" style={{
 						textDecoration: "none",
-						color: "red"
+						color: "red",
 					}}
 						target="_blank"
-						rel="noopener noreferrer">
+						rel="noopener noreferrer"
+						className="stroke">
 						Youtube
 					</a>
 					<a href="https://github.com/dynamic-developments-fps" style={{
 						textDecoration: "none",
-						color: "black"
+						color: "white",
 					}}
 						target="_blank"
-						rel="noopener noreferrer">
+						rel="noopener noreferrer"
+						className="stroke">
 						Github
 					</a>
 				</span>
--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,24 @@ export default defineConfig({
 			"@/": "/src/"
 		}
 	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					'preact': ['preact', 'preact-iso', 'preact/hooks', 'preact/compat'],
+					'home': ["./src/pages/Home/index.tsx", './src/pages/Home/Home.css'],
+					'index': ["./src/index.tsx", "./src/globals.css"],
+					'notfound': ["./src/pages/_404.tsx"],
+					'header': ["./src/components/Header.tsx", "./src/components/Header.css"]
+				}
+			}
+		}
+	},
+	preview: {
+		port: 8000
+	},
+	server: {
+		port: 8000
+	},
 	plugins: [preact()],
 });
